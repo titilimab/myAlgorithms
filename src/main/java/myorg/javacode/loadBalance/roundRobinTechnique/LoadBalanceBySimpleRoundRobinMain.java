5359dc5fae79d3bd37ca419bc9e0b43ce9c75d55
titilima <titilimab@gmail.com>
2020-07-09 13:31:13 -0700

    Added explanation for "synchronized" Block in Java

--- src/main/java/myorg/javacode/loadBalance/roundRobinTechnique/LoadBalanceBySimpleRoundRobinMain.java
+++ src/main/java/myorg/javacode/loadBalance/roundRobinTechnique/LoadBalanceBySimpleRoundRobinMain.java
@@ -48,6 +48,36 @@ interface LoadBalance {
 }
 
 /****************************************************************************************/
+/*
+Synchronized Keyword in in Java:
+
+Multi-threaded programs may often come to a situation where multiple threads try to access the same resources and finally produce erroneous and unforeseen results.
+So it needs to be made sure by some synchronization method that only one thread can access the resource at a given point of time.
+
+->Java provides a way of creating threads and synchronizing their task by using synchronized blocks. 
+->Synchronized blocks in Java are marked with the synchronized keyword. 
+->A synchronized block in Java is synchronized on some object. 
+->All synchronized blocks synchronized on the same object can only have one thread executing inside them at a time. 
+->All other threads attempting to enter the synchronized block are blocked until the thread inside the synchronized block exits the block.
+
+Following is the general form of a synchronized block:
+
+// Only one thread can execute at a time. 
+// sync_object is a reference to an object
+// whose lock associates with the monitor. 
+// The code is said to be synchronized on
+// the monitor object
+synchronized(sync_object)
+{
+   // Access shared variables and other
+   // shared resources
+}
+
+This synchronization is implemented in Java with a concept called monitors. 
+Only one thread can own a monitor at a given time. When a thread acquires a lock, it is said to have entered the monitor. 
+All other threads attempting to enter the locked monitor will be suspended until the first thread exits the monitor.
+
+*/
 
 class RoundRobin implements LoadBalance {
     private static Integer position = 0;
@@ -127,7 +157,7 @@ public class LoadBalanceBySimpleRoundRobinMain {
 	|
 	|
 	|
-	goes on
+	goes on till 100 times from 0 to 99
  
  * */
  
